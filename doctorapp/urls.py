b20bac78d09b5af151755548f4d6d69dae3ebc44
Luis Martinez <lcmartinezl05@gmail.com>
2024-09-03 18:03:41 -0500

    Clase 5, 6, 7: Gestión Completa de Pacientes mediante Vistas Basadas en Funciones

--- doctorapp/urls.py
+++ doctorapp/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
-from patients.views import list_patients
+from django.contrib import admin
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/patients', list_patients)
+    path('api/', include('patients.urls')),
 ]
