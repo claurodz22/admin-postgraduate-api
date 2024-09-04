a434906eb9dffd12c8b843e5de79b769002acebf
Luis Martinez <lcmartinezl05@gmail.com>
2024-09-04 18:15:35 -0500

    Clase 12: Vistas Personalizadas y ViewSets

--- doctors/urls.py
+++ doctors/urls.py
@@ -10,14 +10,17 @@ from .views import (
     ListMedicalNoteView,
     DetailMedicalNoteView,
 )
+from rest_framework.routers import DefaultRouter
+from .viewsets import DoctorViewSet
+
+router = DefaultRouter()
+router.register('doctors', DoctorViewSet)
 
 urlpatterns = [
-    path('doctors/', ListDoctorView.as_view()),
-    path('doctors/<int:id>/', DetailDoctorView.as_view()),
     path('departments/', ListDepartmentView.as_view()),
     path('departments/<int:id>/', DetailDepartmentView.as_view()),
     path('doctoravailabilities/', ListDoctorAvailabilityView.as_view()),
     path('doctoravailabilities/<int:id>/', DetailDoctorAvailabilityView.as_view()),
     path('medicalnotes/', ListMedicalNoteView.as_view()),
     path('medicalnotes/<int:id>/', DetailMedicalNoteView.as_view()),
-]
+] + router.urls
