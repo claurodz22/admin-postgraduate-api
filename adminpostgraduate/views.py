b814daaedde6fb39efb2b5619eba2b376c0d2358
Claudia <claudiaelena091@gmail.com>
2024-12-18 18:34:55 -0400

    api_avance_2

---
+++ adminpostgraduate/views.py
@@ -0,0 +1,22 @@
+
+from rest_framework import serializers, views, status
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class RegisterUserView(views.APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'User created successfully!'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
